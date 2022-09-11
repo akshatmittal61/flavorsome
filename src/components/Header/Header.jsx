92d9ebeaac37ada0d4e2536123206c943583085f
Akshat Mittal <akshatmittal2506@gmail.com>
2022-09-11 17:13:06 +0530

    created header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import navLinks from "../../utils/navigation";
 import socials from "../../utils/socials";
+import "./header.css";
 
 const Header = () => {
 	return (
 		<header className="header">
 			<div className="header-left">
-				{socials.map((social) => (
-					<a href={social.link} target="_blank" rel="noreferrer">
+				{socials.map((social, id) => (
+					<a
+						href={social.link}
+						key={id}
+						target="_blank"
+						rel="noreferrer"
+					>
 						{social.icon}
 					</a>
 				))}
 			</div>
-			<div className="header-mid"></div>
+			<div className="header-mid">
+				<nav>
+					<ul>
+						{navLinks.map((nav, id) => {
+							return (
+								id < navLinks.length / 2 && (
+									<li key={id}>
+										<Link to={nav.route}>{nav.title}</Link>
+									</li>
+								)
+							);
+						})}
+					</ul>
+				</nav>
+				<h1>Meal Time</h1>
+				<nav>
+					<ul>
+						{navLinks.map((nav, id) => {
+							return (
+								id >= navLinks.length / 2 && (
+									<li key={id}>
+										<Link to={nav.route}>{nav.title}</Link>
+									</li>
+								)
+							);
+						})}
+					</ul>
+				</nav>
+			</div>
 			<div className="header-right"></div>
 		</header>
 	);
---
+++ src/utils/navigation.js
@@ -0,0 +1,36 @@
+import { Coffee, Home, Info, Phone, Search, User } from "react-feather";
+
+const navLinks = [
+	{
+		title: "Home",
+		route: "/",
+		icon: <Home />,
+	},
+	{
+		title: "About",
+		route: "/about",
+		icon: <Info />,
+	},
+	{
+		title: "Search",
+		route: "/search",
+		icon: <Search />,
+	},
+	{
+		title: "Recipes",
+		route: "/recipes",
+		icon: <Coffee />,
+	},
+	{
+		title: "Profile",
+		route: "/profile",
+		icon: <User />,
+	},
+	{
+		title: "Contact Us",
+		route: "/contact",
+		icon: <Phone />,
+	},
+];
+
+export default navLinks;
