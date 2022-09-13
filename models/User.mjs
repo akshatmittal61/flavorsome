04bd832358d82668e9804e8148f19dc9fb3e98c1
Akshat Mittal <akshatmittal2506@gmail.com>
2022-09-13 12:25:47 +0530

    created user model

---
+++ models/User.mjs
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+	{
+		fname: {
+			type: String,
+			required: true,
+		},
+		lname: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		avatar: {
+			type: String,
+			default:
+				"https://raw.githubusercontent.com/akshatmittal61/planner/master/src/images/user.svg",
+		},
+		phone: {
+			type: String,
+			required: false,
+		},
+		bio: {
+			type: String,
+			required: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+const User = mongoose.model("user", UserSchema);
+export default User;
--- server.mjs
+++ server.mjs
@@ -11,9 +11,6 @@ app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
-});
 
 app.listen(PORT, () => {
 	connect();
